b90062b5c9e8ecd89b7847367cdc9950317d6bb1
vitaliiMn <115878020+vitaliiMn@users.noreply.github.com>
2025-06-01 17:09:56 +0300

    Update helpers.py

---
+++ utils/helpers.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from pathlib import Path
+
+DATA_PATH = Path("../data_lake/csv_data/2025-06-01") / "csv_data_2025-06-01.csv"
+
+def load_client_data():
+    """Загружает данные клиентов из CSV"""
+    return pd.read_csv(DATA_PATH, delimiter='\t')
